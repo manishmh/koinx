c4b76af2db9841c4edef5ea984d5349f3181f528
manishmh <manishmh982@gmail.com>
2024-03-08 01:23:47 +0530

    added easy coin navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,15 @@ export default function Home() {
       <Link href="/cryptocurrencies/bitcoin">
         <button className="px-4 py-2 m-8 bg-blue-700 rounded-md text-white">Track cryptocurrencies</button>
       </Link>
+      <Link href="/cryptocurrencies/bitcoin">
+        <button className="px-4 py-2 m-8 bg-blue-700 rounded-md text-white">Bitcoin</button>
+      </Link>
+      <Link href="/cryptocurrencies/ethereum">
+        <button className="px-4 py-2 m-8 bg-blue-700 rounded-md text-white">Ethereum</button>
+      </Link>
+      <Link href="/cryptocurrencies/dogecoin">
+        <button className="px-4 py-2 m-8 bg-blue-700 rounded-md text-white">Dogecoin</button>
+      </Link>
     </div>
   );
 }
